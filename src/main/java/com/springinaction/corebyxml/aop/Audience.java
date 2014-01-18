7f0436db2338f08115f7383e5d547f9d6478a30f
Vladislav Shynkarenko <vadic7wee2@gmail.com>
2014-01-18 16:44:14 +0200

    Added AOP around example

--- src/main/java/com/springinaction/corebyxml/aop/Audience.java
+++ src/main/java/com/springinaction/corebyxml/aop/Audience.java
@@ -1,5 +1,7 @@
 package com.springinaction.corebyxml.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class Audience {
 
     public void takeSeats() {
@@ -18,4 +20,18 @@ public class Audience {
         System.out.println("Boo! We want our money back!");
     }
 
+    public void watchPerformance(ProceedingJoinPoint joinpoint) {
+        try {
+            System.out.println("Around - Before perform is executed");
+            long start = System.currentTimeMillis();
+
+            joinpoint.proceed();
+
+            long end = System.currentTimeMillis();
+            System.out.println("Around - After perform is executed");
+            System.out.println("The performance took " + (end - start) + " milliseconds.");
+        } catch (Throwable t) {
+            System.out.println("Boo! We want our money back!");
+        }
+    }
 }
